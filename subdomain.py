0e39c9fd96ef59eb8e8c15faa95ec1c01a7accc1
Bagus Dwi Kurniawan <77946920+bagusdwik@users.noreply.github.com>
2024-01-11 09:58:53 +0700

    subdomain.py

--- subdomain.py
+++ subdomain.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-main_domain = "<YOUR_DOMAIN>"
+main_domain = "umsida.ac.id"
 response = requests.get(f"https://{main_domain}")
 
 soup = BeautifulSoup(response.content, "html.parser")
@@ -10,8 +10,11 @@ subdomains = set()
 for link in soup.find_all("a", href=True):
     href = link["href"]
     if main_domain in href:
-        subdomain = href.split("//")[1].split(".")[0]
-        subdomains.add(subdomain)
+        parts = href.split("//")
+        if len(parts) > 1:
+            subdomain = parts[1].split(".")[0]
+            subdomains.add(subdomain)
+
 print("Subdomains:")
 for subdomain in subdomains:
     print(subdomain)
